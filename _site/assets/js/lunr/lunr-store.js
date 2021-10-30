271224e5165fd8f980efe6f0bf2bef2080561922
John Gibbons <john.gibbons@xsellreward.com>
2021-10-31 10:31:07 +1100

    Cleanup files

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "Welcome to Nial_Development",
-        "excerpt":"Welcome to the new Nial development website.   This site is a fork of the QNial7 distribution of Nial and is intended to be a base for further development of the Nial programming language.  ","categories": [],
-        "tags": ["Nial"],
-        "url": "/2021-10-24-welcome-to-nial/welcome-to-nial.html",
-        "teaser": null
-      }]
